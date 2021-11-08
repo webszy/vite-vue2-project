b4aa509919847339fe5f433727b1b97b1a65b66a
孙振宇 <sunzhenyu@live.com>
2021-11-08 18:06:08 +0800

    update:remove the dep:@vue/compostion-api

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
-// import Vue from 'vue'
+import Vue from 'vue'
 import App from '@/App.vue'
-import { createApp, h, install,Vue2 } from "vue-demi"
+import { createApp, h, install } from "vue-demi"
 import store from './store'
 import 'virtual:windi.css'
 import router from '@/router'
 install()
-import VueCompositionApi from '@vue/composition-api'
-Vue2.use(VueCompositionApi)
-Vue2.config.productionTip = false
-Vue2.config.devtools = true
+Vue.config.productionTip = false
+Vue.config.devtools = true
 const app = createApp({
     router,
     store,
